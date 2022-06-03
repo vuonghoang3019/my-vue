17f4971ce6704ddd335ebfe635dd1297d6011443
vuonghoang3019 <vuonghk1@gmail.com>
2022-06-03 17:20:18 +0700

    fix layout register

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,32 +1,39 @@
 <template>
   <div class="mt-8">
     <div class="container mx-auto px-8">
-      <form @submit="onSubmit" class="">
+      <form @submit="onSubmit" class="flex flex-col justify-start space-y-6">
         <div class="row">
-          <label for="fullName">
+          <label for="fullName" class="flex flex-col">
             <span class="font-semibold">Full name</span>
             <input type="text" placeholder="Test..." 
             id="fullName"
-            class="px-4 py-3 rounded-lg border-gray-100 mt-1" />
+            class="px-4 py-3 rounded-lg border border-gray-300 mt-1" />
           </label>
         </div>
         <div class="row">
-          <label for="fullName">
-            <span class="font-semibold">Full name</span>
-            <input type="text" placeholder="Test..." 
-            id="fullName"
-            class="px-4 py-3 rounded-lg border-gray-100 mt-1 mx-10" />
+          <label for="email" class="flex flex-col">
+            <span class="font-semibold">Email</span>
+            <input type="email" placeholder="Test..." 
+            id="email"
+            class="px-4 py-3 rounded-lg border border-gray-300 mt-1" />
           </label>
         </div>
         <div class="row">
-          <label for="fullName">
-            <span class="font-semibold">Full name</span>
-            <input type="text" placeholder="Test..." 
-            id="fullName"
-            class="px-4 py-3 rounded-lg border-gray-100 mt-1" />
+          <label for="password" class="flex flex-col">
+            <span class="font-semibold">Password</span>
+            <input type="password" placeholder="Test..." 
+            id="password"
+            class="px-4 py-3 rounded-lg border border-gray-300 mt-1" />
           </label>
+        </div>
+        <div class="row">
+          <button type="submit" class="py-3 text-center w-full bg-primary text-white font-bold rounded-lg">Sign up</button>
         </div>
       </form>
+      <div class="w-full text-center mt-6">
+        <span class="font-semibold">I'm already a member.</span>
+        <!-- <span class="ml-1"><router-link to={ name: 'Login', params: {} }>Sign In</router-link></span> -->
+      </div>
     </div>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark: '#363853',
+        primary: '#0012FF',
+        green: '#67D4CA',
+        red: '#F28080'
+      }
+    },
   },
   plugins: [],
 }
