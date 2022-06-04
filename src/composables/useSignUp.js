146dd48704d7b96f20efb4223f811b03adbc6b20
vuonghoang3019 <vuonghk1@gmail.com>
2022-06-04 11:45:22 +0700

    add composables

---
+++ src/composables/useSignUp.js
@@ -0,0 +1,21 @@
+import { ref } from "vue"
+import { projectAuth } from "@/configs/firebase"
+const error = ref(null)
+const isPending = ref(false)
+
+async function useSignUp(email, password, fullName) {
+	error.value = null;
+	try {
+		const response =   await projectAuth.createUserWithEmailAndPassword(email, password);
+		if (!response) throw new Error("Could not create a new user");
+	
+		return response;
+	}
+	catch (err) {
+		
+	}
+}
+
+export function useSignUp() {
+  return {  }
+}
